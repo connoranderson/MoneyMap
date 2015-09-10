f7a68de0be6154da25c5eeb60fa63147b927b2d1
connoranderson <connora@stanford.edu>
2015-09-10 14:16:52 -0700

    Validation bug fixed

--- assets/js/validate_input.js
+++ assets/js/validate_input.js
@@ -6,30 +6,23 @@ function validateForm() {
   var start_age = parseInt(document.forms["analyze"]["start"].value);
   var end_age = parseInt(document.forms["analyze"]["retirement"].value);
 
-  alert(start_age.toString());  
-  alert(end_age.toString());
-
   if(end_age<= start_age){
     alert("Make sure you pick a retirement age after your current age!");
     return false;
   }
 
-	for(var i = 0; i < elements.length; i++)
-    alert("In loop");
-	{
-		if (isNumeric(elements[i].value) && elements[i].getAttribute('type').toLowerCase() != "checkbox" && elements[i].getAttribute('type').toLowerCase() != "radio" && elements[i].getAttribute('type').toLowerCase() != "submit") {
-			alert(elements[i].getAttribute('name').toLowerCase());
-			alert("Make sure you are submitting valid numbers!");
+	for(var i = 0; i < elements.length; i++)	{
+		if (!isNumeric(elements[i].value) && elements[i].getAttribute('type').toLowerCase() != "checkbox" && elements[i].getAttribute('type').toLowerCase() != "radio" && elements[i].getAttribute('type').toLowerCase() != "submit") {
+			alert("Make sure you submit valid numbers (no letters, just numbers and decimals)");
 			return false;
 		}
 	}
 
 	return true;
 }
-
 function isNumeric(n) {
-  // n = correctPercentageInput(correctIntInput(n));
-  return !isNaN(partseInt(n) || parseFloat(n)) && isFinite(n);
+  n = correctPercentageInput(correctIntInput(n)); // Turns numbers with dollar signs or percentage signs into regular doubles
+  return (!isNaN(parseInt(n) || parseFloat(n)) && isFinite(n));
 }
 
 function correctIntInput(input){
